ef5c2a06815377ab35cf2bdcb8f97abf3663eb97
hanliong-looi <hanliong99@gmail.com>
2021-11-01 22:26:08 +0800

    most annotations done, need to make hash query more specific

--- annotation.py
+++ annotation.py
@@ -3,8 +3,29 @@ class queryAnnotator:
         # Maps each node to its respective annotation
         self.queryMapper = {
             "Hash Join" : hash_join_annotator,
+            "Nested Loop" : nested_loop_annotator,
+            "Merge Join" : merge_join_annotator,
             "Seq Scan" : seq_scan_annotator,
-            "Hash" : hash_annotator
+            "Index Scan" : index_scan_annotator,
+            "Index Only Scan" : index_scan_annotator,
+            "Bitmap Heap Scan" : bitmap_scan_annotator,
+            "Bitmap Index Scan" : bitmap_scan_annotator,
+            "BitmapAnd" : bitmap_scan_annotator,
+            "BitmapOr" : bitmap_scan_annotator,
+            "Values Scan" : value_scan_annotator,
+            "CTE Scan" : cte_scan_annotator,
+            "Function Scan" : function_scan_annotator,
+            "Subquery Scan" : subquery_scan_annotator,
+            "Hash" : hash_annotator,
+            "Sort" : sort_annotator,
+            "Append" : append_annotator,
+            "Aggregate" : aggregate_annotator,
+            "Materialize" : materialize_annotator,
+            "Unique" : unique_annotator,
+            "Limit" : limit_annotator,
+            "Result" : result_annotator,
+            "Gather" : gather_annotator,
+            "Gather Merge" : gather_annotator
         }
 
     # Reads the query plan and sorts the order of query execution
@@ -46,6 +67,9 @@ class queryAnnotator:
         while len(queryOrder) > 0:
             # Get annotation for current query
             queryNode = queryOrder.pop()
+            if queryNode["Node Type"] not in self.queryMapper:
+                print("Cannot find annotation for this query: {}".format(queryNode["Node Type"]))
+                continue
             result.append(self.queryMapper[queryNode["Node Type"]](queryNode))
         return result
     
@@ -115,7 +139,7 @@ def seq_scan_annotator(queryNode):
 
     return annotation
 
-# Function to annotate Index Scan
+# Function to annotate Index Scan or Index Only Scan
 def index_scan_annotator(queryNode):
     scan_name = "Index Scan"
 
@@ -136,16 +160,175 @@ def index_scan_annotator(queryNode):
 
     return annotation
 
+# Function to annotate Bitmap Scan (includes Bitmap Heap Scan, Bitmap Index Scan, BitmapAnd, BitmapOr)
+def bitmap_scan_annotator(queryNode):
+    scan_name = queryNode["Node Type"]
+
+    # If Node Type is Bitmap Heap Scan
+    if "Heap" in scan_name:
+        # Get name of relation
+        relation_name = queryNode["Relation Name"]
+
+        # Get recheck condition if any
+        recheck_condition = "" if "Recheck Cond" not in queryNode else "with recheck condition " + queryNode["Recheck Cond"]
+
+        # Build annotation string
+        annotation = "{} on table {} {}".format(scan_name, relation_name, recheck_condition)
+
+        return annotation
+    
+    # If Node Type is Bitmap Index Scan
+    elif "Index" in scan_name:
+        # Get name of index attribute
+        index_name = queryNode["Index Name"]
+
+        # Get index condition if any
+        index_condition = "" if "Index Cond" not in queryNode else "with index condition " + queryNode["Index Cond"]
+
+        # Build annotation string
+        annotation = "{} on table {} {}".format(scan_name, index_name, index_condition)
+
+        return annotation
+    
+    # If Node Type is BitmapAnd or BitmapOr
+
+# Function to annotate Value Scan
+def value_scan_annotator(queryNode):
+    scan_name = queryNode["Node Type"]
+
+    # Get names of values that were scanned on
+    values_name = "on values " + queryNode["Alias"]
+
+    # Build annotation string
+    annotation = "{} {}".format(scan_name, values_name)
+
+    return annotation
+
+# Function to annotate Cte Scan
+def cte_scan_annotator(queryNode):
+    name = "CTE Scan"
+
+    # Get names of relation
+    relation_name = queryNode["Alias"]
+
+    # Build annotation string
+    annotation = "{} on temporary table {}".format(name, relation_name)
+
+    return annotation
+
+# Function to annotate Function Scan
+def function_scan_annotator(queryNode):
+    name = "Function Scan"
+
+    # Get name of function
+    function_name = queryNode["Function Name"]
+
+    # Build annotation string
+    annotation = "{} on function {}".format(name, function_name)
+
+    return annotation
+
+# Function to annotate Subquery Scan
+def subquery_scan_annotator(queryNode):
+    name = "Subquery Scan"
+
+    # Get name of subquery if any
+    subquery_name = "" if "Alias" not in queryNode else "on the subquery '{}'".format(queryNode["Alias"])
+
+    # Get name of filter if any
+    filter_cond = "" if "Filter" not in queryNode else "with the filter ".format(queryNode["Filter"])
+
+    # Build annotation string
+    annotation = "{} {} {}".format(name, subquery_name, filter_cond)
+
+    return annotation
+
+
+
 # Function to annotate Hash
 def hash_annotator(queryNode):
     name = "Hash"
     
-    relation_name = "on table {}".format(queryNode["Plans"][0]["Relation Name"])
+    # Get name of relation if any
+    relation_name = "" if "Relation Name" not in queryNode else "on table {}".format(queryNode["Plans"][0]["Relation Name"])
 
-    # Get hash condition
+    # Get hash condition if any
     condition = "" if "Hash Cond" not in queryNode else "on condition " + queryNode["Hash Cond"]
 
     # Build annotation string
     annotation = "{} {} {}".format(name, relation_name, condition)
+
+    return annotation
+
+# Function to annotate Sort
+def sort_annotator(queryNode):
+    name = "Sort"
+
+    # Get name of keys sorted on
+    sort_keys = queryNode["Sort Key"]
+
+    # Separate keys by commas
+    sort_key_annotation = ""
+    for i in range(len(sort_keys)):
+        if i > 0:
+            sort_key_annotation += ", "
+        sort_key_annotation += sort_keys[i]
+    
+    # Build annotation string
+    annotation = "{} based on {}".format(name, sort_key_annotation)
+
+    return annotation
+
+# Function to annotate Append
+def append_annotator():
+    return "Append operation"
+
+# Function to annotate Aggregate
+def aggregate_annotator(queryNode):
+    name = "Aggregate"
+
+    # Get aggregate condition 
+    if "Group Key" in queryNode:
+        aggregate_cond = "based on the table " + queryNode["Group Key"][0]
+    else:
+        aggregate_cond = "to get " + queryNode["Output"][0]
+
+    # Build annotation string
+    annotation = "{} {}".format(name, aggregate_cond)
+
+    return annotation
+
+# Function to annotate Materialize
+def materialize_annotator():
+    return "Materialize output into memory"
+
+# Function to annotate Unique
+def unique_annotator():
+    return "Remove duplicate values"
+
+# Function to annotate Limit
+def limit_annotator(queryNode):
+    num_rows = queryNode["Plan Rows"]
+
+    # Build annotation string
+    annotation = "Limit to the top {} rows".format(num_rows)
+
+    return annotation
+
+# Function to annotate Result
+def result_annotator(queryNode):
+    num_rows = queryNode["Plan Rows"]
+
+    # Build annotation string
+    annotation = "{} number of row(s) are obtained as the result".format(num_rows)
+
+    return annotation
+
+# Function to annotate Gather or Gather Merge
+def gather_annotator(queryNode):
+    name = queryNode["Node Type"]
+
+    # Build annotation string
+    annotation = "{} on the output".format(name)
 
     return annotation
